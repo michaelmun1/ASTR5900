ed8d344e0341ff9c96e0be715e87baed877f4998
Michael Munnelly <michael.munnelly-1@ou.edu>
2026-02-03 09:59:24 -0600

    Add greet function

--- HW00/demo.py
+++ HW00/demo.py
@@ -1,7 +1,10 @@
 # Homework 00 demo file
 
+def greet(name):
+    return f"Hello, {name}"
+
 def main():
-    print("Hello world")
+    print(greet("Mike"))
 
 if __name__ == "__main__":
     main()
